bde5efc0e741c73f70186345b7f7142e0207b434
CodeEventHorizon <nikoloz.9374@gmail.com>
2023-04-17 23:09:08 +0400

    add: connect to the MongoDB

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductModule } from './product/product.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/amazon'),
+    ProductModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
