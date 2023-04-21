429c074879526a3ae3e3a104b9ba0268ee6b3a84
CodeEventHorizon <nikoloz.9374@gmail.com>
2023-04-21 23:10:36 +0400

    add: user schema containing name, email and password

---
+++ api/src/user/user.schema.ts
@@ -0,0 +1,15 @@
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+
+export type UserDocument = User & Document;
+
+@Schema()
+export class User {
+  @Prop({ required: true })
+  name: string;
+  @Prop({ required: true, unique: true })
+  email: string;
+  @Prop({ required: true })
+  password: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
