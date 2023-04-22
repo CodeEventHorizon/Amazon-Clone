3330764f6b7ad3c75a9db70b1b1ae7a072f3c8d8
CodeEventHorizon <nikoloz.9374@gmail.com>
2023-04-23 01:14:59 +0400

    add: user service get user details and create user

--- api/src/user/user.service.ts
+++ api/src/user/user.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { UserDocument } from './user.schema';
+import { UserDetails } from './user-details.interface';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectModel('User') private readonly userModel: Model<UserDocument>,
+  ) {}
+
+  _getUserDetails(user: UserDocument): UserDetails {
+    return {
+      id: user._id,
+      name: user.name,
+      email: user.email,
+    };
+  }
+
+  async create(
+    name: string,
+    email: string,
+    hashedPassword: string,
+  ): Promise<UserDocument> {
+    const newUser = new this.userModel({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    return newUser.save();
+  }
+}
