4b2bfe7303e2425951679c6f8d7a41535ea09e99
CodeEventHorizon <nikoloz.9374@gmail.com>
2023-04-23 20:36:08 +0400

    add: auth service register and hash password

--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
+import { NewUserDTO } from 'src/user/dtos/new-user.dto';
+import { UserDetails } from 'src/user/user-details.interface';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private userService: UserService) {}
+
+  async hashPassword(password: string): Promise<string> {
+    return bcrypt.hash(password, 12);
+  }
+
+  //! better use error handling instead of returning null
+  async register(user: Readonly<NewUserDTO>): Promise<UserDetails | any> {
+    const { name, email, password } = user;
+
+    const existingUser = await this.userService.findByEmail(email);
+
+    if (existingUser) return 'Email taken!';
+
+    const hashedPassword = await this.hashPassword(password);
+
+    const newUser = await this.userService.create(name, email, hashedPassword);
+
+    return this.userService._getUserDetails(newUser);
+  }
+}
