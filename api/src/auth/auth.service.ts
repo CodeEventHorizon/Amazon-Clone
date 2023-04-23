4538a950f859a69f6d45ce214599c40441a4b507
CodeEventHorizon <nikoloz.9374@gmail.com>
2023-04-24 01:38:27 +0400

    add: auth service, login service
    This commit adds doesPasswordMatch, validateUser and login async methods

--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -3,10 +3,15 @@ import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcrypt';
 import { NewUserDTO } from 'src/user/dtos/new-user.dto';
 import { UserDetails } from 'src/user/user-details.interface';
+import { ExistingUserDTO } from 'src/user/dtos/existing-user.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
   async hashPassword(password: string): Promise<string> {
     return bcrypt.hash(password, 12);
@@ -25,5 +30,43 @@ export class AuthService {
     const newUser = await this.userService.create(name, email, hashedPassword);
 
     return this.userService._getUserDetails(newUser);
+  }
+
+  async doesPasswordMatch(
+    password: string,
+    hashedPassword: string,
+  ): Promise<boolean> {
+    return bcrypt.compare(password, hashedPassword);
+  }
+
+  async validateUser(
+    email: string,
+    password: string,
+  ): Promise<UserDetails | null> {
+    const user = await this.userService.findByEmail(email);
+    const doesUserExist = !!user;
+
+    if (!doesUserExist) return null;
+
+    const doesPasswordMatch = await this.doesPasswordMatch(
+      password,
+      user.password,
+    );
+
+    if (!doesPasswordMatch) return null;
+
+    return this.userService._getUserDetails(user);
+  }
+
+  async login(
+    existingUser: ExistingUserDTO,
+  ): Promise<{ token: string } | null> {
+    const { email, password } = existingUser;
+    const user = await this.validateUser(email, password);
+
+    if (!user) return null;
+
+    const jwt = await this.jwtService.signAsync({ user });
+    return { token: jwt };
   }
 }
