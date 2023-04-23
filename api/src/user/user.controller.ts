71fb2e6e4bdc02ff23df672300105cec061cfa3f
CodeEventHorizon <nikoloz.9374@gmail.com>
2023-04-24 01:39:39 +0400

    add: user controller Get user by id request

--- api/src/user/user.controller.ts
+++ api/src/user/user.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { UserService } from './user.service';
+import { UserDetails } from './user-details.interface';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Get(':id')
+  getUser(@Param('id') id: string): Promise<UserDetails | null> {
+    return this.userService.findById(id);
+  }
+}
