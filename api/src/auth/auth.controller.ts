979ad44060f3135e9dc6a53069bd25f9ef8f4778
CodeEventHorizon <nikoloz.9374@gmail.com>
2023-04-24 00:04:06 +0400

    add: auth controller register

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { NewUserDTO } from 'src/user/dtos/new-user.dto';
+import { UserDetails } from 'src/user/user-details.interface';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+  @Post('register')
+  register(@Body() user: NewUserDTO): Promise<UserDetails | null> {
+    return this.authService.register(user);
+  }
+}
